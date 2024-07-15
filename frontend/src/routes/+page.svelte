10e26ae17f387666e6d0dfb31302d5adf8bb63ee
Denis Shumeev <denis-shumeev@yandex.ru>
2024-07-15 19:38:31 +0300

    Work on refresh button 2.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -134,7 +134,7 @@
     {#if filteredBrands.length}
       {#each filteredBrands as brand (brand.name)}
         <tr data-brand-name={brand.name} class="bg-white even:bg-slate-100">
-          <td class="p-2"><a href="/{brand.name}" class="underline">{brand.name}</a></td>
+          <td class="p-2"><a href="/{brand.name}" target="_blank" class="underline">{brand.name}</a></td>
           <td class="p-2 text-wrap">
             <Hosts bind:hosts={brand.hosts}/>
           </td>
